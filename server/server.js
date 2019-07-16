95190a6d8807b575e826c4b6aaf22258b7db37ad
Ricardo <ricardoarrobo@gmail.com>
2019-07-16 10:08:16 -0500

    cors enabled

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ require('./config/config');
 const app = require('express')();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const router = require('./routes/router');
 
 const { userRoute } = require('./routes/user');
+
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(cors());
 app.use(router);
 
 
